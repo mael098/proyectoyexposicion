b3db245ae78fe0ccc0405ac2bf967ae876469985
mael098 <juanvillegasmorales@outlook.com>
2023-11-21 23:46:37 -0600

    barra de navegacion

--- src/App.jsx
+++ src/App.jsx
@@ -1,53 +1,24 @@
 import "./App.css"
-import Logo from "./assets/react.svg"
 
 function App() {
   return (
     <>
-      <nav>
-        <ul>
-          <li>
-            <a href="">
-              <img src={Logo} alt="" />
-            </a>
-          </li>
-          <li>
-            <span className="material-symbols-outlined">explore_nearby</span>
-            <p>actualizar ubicasion</p>
-            <p>entregar en mexico city 11000</p>
-          </li>
-          <li>
-            <section></section>
-            <input type="text" />
-            <input type="button" value="" />
-          </li>
-          <li>
-            <div>
-              <a href="">
-                <span className="material-symbols-outlined">hola indentificate</span>
-                <p>cuenta y listas</p>
-              </a>
-            </div>
-          </li>
-          <li>
-            <div>
-              <a href="">
-                <span className="material-symbols-outlined">hola indentificate</span>
-                <p>cuenta y listas</p>
-              </a>
-            </div>
-          </li>
-          <li>
-            <div>
-              <a href="">
-                <span className="material-symbols-outlined">shopping_cart</span>
-                <span>0</span>
-                <span>carrito</span>
-              </a>
-            </div>
-          </li>
-        </ul>
-      </nav>
+     <nav>
+      <div className="logo">
+        <img src="https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.klipartz.com%2Fes%2Fsticker-png-yjbou&psig=AOvVaw2gVGoiQnLvDkC7hrxgSZR8&ust=1700718048417000&source=images&cd=vfe&opi=89978449&ved=0CBEQjRxqFwoTCODvnsvy1oIDFQAAAAAdAAAAABAJ" alt="" />
+      </div>
+      <div className="search-bar">
+        <input type="text" />
+        <button type="button">Buscar</button>
+      </div>
+      <div className="nav-links">
+        <a href="#">hoy´s deals</a>
+        <a href="#">Tarjetas Regalo</a>
+        <a href="#">Vender</a>
+        <a href="#">Ayuda</a>
+      </div>
+    </nav>
+
       <section></section>
     </>
   )
